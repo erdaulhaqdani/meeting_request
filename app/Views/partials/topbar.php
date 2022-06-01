a5b51c328b5a6ab3e4aff91c977b8a6527a33ff5
Reggi Fachri <reggifachri@student.telkomuniversity.ac.id>
2022-06-01 11:49:28 +0700

    penyesuaian dropdown pada topbar

--- app/Views/partials/topbar.php
+++ app/Views/partials/topbar.php
@@ -222,10 +222,6 @@
                     <?php elseif (session('NIP') == TRUE) : ?>
                         <a class="dropdown-item" href="/admin/profile"><i class="ri-user-line align-middle me-1"></i> Profile</a>
                     <?php endif ?>
-                    <a class="dropdown-item" href="#"><i class="ri-wallet-2-line align-middle me-1"></i> My Wallet</a>
-                    <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
-                    <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
-                    <div class="dropdown-divider"></div>
                     <a class="dropdown-item text-danger" href="/logout_cust"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
                 </div>
             </div>
