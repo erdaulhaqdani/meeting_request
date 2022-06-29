6612aff3cfbfb7056171e6fbc85479a2d10ce3bb
Reggi Fachri <reggifachri@student.telkomuniversity.ac.id>
2022-06-29 17:19:56 +0700

    fitur verifikasi akun

--- app/Views/landing_page/detail_customer.php
+++ app/Views/landing_page/detail_customer.php
@@ -120,6 +120,7 @@
                                     <div class="row">
                                         <div class="col-sm-5">
                                             <input type="button" value="Kembali" class="btn btn-warning waves-effect mt-2" onclick="history.back(-1)" />
+                                            <a href="/detail_customer/verifikasi<?= $customer['idCustomer']; ?>" class="btn btn-info btn-sm w-xs">Verifikasi</a>
                                         </div>
                                     </div>
 
