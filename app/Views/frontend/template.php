2cfd4a8ee54c19fda79afc373974e83ad0e8edb7
Reggi Fachri <reggifachri@student.telkomuniversity.ac.id>
2022-06-30 11:39:49 +0700

    base url pada template

--- app/Views/frontend/template.php
+++ app/Views/frontend/template.php
@@ -11,21 +11,21 @@
 
   <!-- Favicons-->
   <link rel="icon" type="image/png" href="<?= base_url('assets/images/favicon-32x32.png'); ?>" sizes="32x32" />
-  <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-  <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-  <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-  <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+  <link rel="apple-touch-icon" type="image/x-icon" href="<?= base_url('img/apple-touch-icon-57x57-precomposed.png'); ?>">
+  <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="<?= base_url('img/apple-touch-icon-72x72-precomposed.png'); ?>">
+  <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="<?= base_url('img/apple-touch-icon-114x114-precomposed.png'); ?>">
+  <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="<?= base_url('img/apple-touch-icon-144x144-precomposed.png'); ?>">
 
   <!-- GOOGLE WEB FONT -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
   <!-- BASE CSS -->
-  <link href="/login_regis/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/login_regis/css/vendors.css" rel="stylesheet">
-  <link href="/login_regis/css/style.css" rel="stylesheet">
+  <link href="<?= base_url('/login_regis/css/bootstrap.min.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('/login_regis/css/vendors.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('/login_regis/css/style.css'); ?>" rel="stylesheet">
 
   <!-- YOUR CUSTOM CSS -->
-  <link href="/login_regis/css/custom.css" rel="stylesheet">
+  <link href="<?= base_url('/login_regis/css/custom.css'); ?>" rel="stylesheet">
 
 </head>
 
